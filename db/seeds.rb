97e77e04439faa2a9bcf1e6656571ee6c2e27386
Nicholas Utikal <nicholasutikal@gmail.com>
2018-02-18 14:37:06 +0100

    now you can add simple items/cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+
+cocktails = %w(mojito vodkaOrange CubaLibre Pinacolada VodkaEnergy GinTonic)
+cocktails.each { |cocktail| Cocktail.create(name: cocktail) }
+
+
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
+
+
+
+
